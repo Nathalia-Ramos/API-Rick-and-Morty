90f0ec081c84f31fb9fcd4346992bdde264b8302
Nathalia Ramos <nath.ramos@gmail.com>
2022-06-04 22:22:32 -0300

    falta arrumar os cards

--- JS/main.js
+++ JS/main.js
@@ -14,12 +14,16 @@ fetch (url,
 
     //Essa função serve para exibir por meio do innerHTML o que tiver na função do container
     json.results.map(function(results){
+        
         container.innerHTML+=`
         
-            <div> <img src= `+ results.image + `> </div>
-            <hr>
-
-            <strong>  `+ results.name + `</strong>    
+            <div> 
+               <img src= `+ results.image + ` > 
+               <h3> <strong> Name: `+ results.name + `</strong> </h3>
+               <h5><strong> Status: `+ results.status + `</strong><h5>  
+               <h5><strong>Gender: `+ results.gender + `</strong><h5> 
+               <h5><strong> Species: `+ results.species + `</strong><h5>
+            </div>
         `;
 
     })
